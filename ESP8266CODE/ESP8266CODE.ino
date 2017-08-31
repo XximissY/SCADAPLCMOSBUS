9d53e2c3cbc736efb8b2d19733c666afef63b7dd
XximissY <kiratisreesamran@gmail.com>
2017-08-31 16:24:50 +0700

    Add files via upload

---
+++ ESP8266CODE/ESP8266CODE.ino
@@ -0,0 +1,261 @@
+#include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3f, 16, 2);
+#define LED LED_BUILTIN;
+
+const char* ssid = "ESP_IOT";
+const char* password = "123456789";
+unsigned char status_led = 0;
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("");
+  lcd.begin();
+  delay(10);
+
+
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, 0);
+
+  // Connect to WiFi network
+  lcd.clear();
+  lcd.print("Connecting to ");
+  lcd.setCursor(0, 1);
+  lcd.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+
+  }
+  lcd.setCursor(0, 0);
+  lcd.println("WiFi connected");
+
+  // Start the server
+  server.begin();
+  lcd.setCursor(0, 0);
+  lcd.println("Server started");
+
+  // Print the IP address
+  lcd.clear();
+  lcd.print(" ");
+  lcd.println(WiFi.localIP());
+  lcd.setCursor(0, 1);
+  Serial.println(WiFi.localIP());
+}
+
+void loop() {
+  // Check if a client has connected
+  WiFiClient client = server.available();
+  if (!client) {
+    return;
+  }
+
+  // Wait until the client sends some data
+  lcd.println("new client");
+  while (!client.available()) {
+    delay(1);
+  }
+
+  // Read the first line of the request
+  String req = client.readStringUntil('\r');
+  lcd.setCursor(0, 1);
+  lcd.print(req);
+  client.flush();
+
+  // Match the request
+  int i = 0;
+  int val = 0;
+  //////////////////////////WETBLUE/////////////////////////
+  if (req.indexOf("/wetblue/1") != -1) {
+    val = 1;
+    Serial.print("lp01");
+  } else if (req.indexOf("/wetblue/2") != -1) {
+    val = 2;
+    Serial.print("lp03");
+  } else if (req.indexOf("/wetblue/3") != -1) {
+    val = 3;
+    Serial.print("lp05");
+  } else if (req.indexOf("/wetblue/4") != -1) {
+    val = 4;
+    Serial.print("lp07");
+  } else if (req.indexOf("/wetblue/5") != -1) {
+    val = 5;
+    Serial.print("lp09");
+  } else if (req.indexOf("/wetblue/6") != -1) {
+    val = 6;
+    Serial.print("lp11");
+  } else if (req.indexOf("/wetblue/7") != -1) {
+    val = 7;
+    Serial.print("lp13");
+  } else if (req.indexOf("/wetblue/8") != -1) {
+    val = 8;
+    Serial.print("lp15");
+  } else if (req.indexOf("/wetblue/9") != -1) {
+    val = 9;
+    Serial.print("lp17");
+  } else if (req.indexOf("/wetblue/A") != -1) {
+    val = 10;
+    Serial.print("lp19");
+  } else if (req.indexOf("/wetblue/PB7") != -1) {
+    val = 11;
+    Serial.print("ep27");
+  } else if (req.indexOf("/wetblue/PB9") != -1) {
+    val = 12;
+    Serial.print("ep29");
+  } else
+    //////////////////////////Crush////////////////////////////
+
+    if (req.indexOf("/crush/1") != -1) {
+      val = 1;
+      Serial.print("lp02");
+    } else if (req.indexOf("/crush/2") != -1) {
+      val = 2;
+      Serial.print("lp04");
+    } else if (req.indexOf("/crush/3") != -1) {
+      val = 3;
+      Serial.print("lp06");
+    } else if (req.indexOf("/crush/4") != -1) {
+      val = 4;
+      Serial.print("lp08");
+    } else if (req.indexOf("/crush/5") != -1) {
+      val = 5;
+      Serial.print("lp10");
+    } else if (req.indexOf("/crush/6") != -1) {
+      val = 6;
+      Serial.print("lp12");
+    } else if (req.indexOf("/crush/7") != -1) {
+      val = 7;
+      Serial.print("lp14");
+    } else if (req.indexOf("/crush/8") != -1) {
+      val = 8;
+      Serial.print("lp16");
+    } else if (req.indexOf("/crush/9") != -1) {
+      val = 9;
+      Serial.print("lp18");
+    } else if (req.indexOf("/crush/A") != -1) {
+      val = 10;
+      Serial.print("lp28");
+    } else if (req.indexOf("/crush/B") != -1) {
+      val = 11;
+      Serial.print("lp32");
+    } else if (req.indexOf("/crush/C") != -1) {
+      val = 12;
+      Serial.print("lp38");
+    } else if (req.indexOf("/crush/D") != -1) {
+      val = 13;
+      Serial.print("lp41");
+    } else  if (req.indexOf("/crush/P2") != -1) {
+      val = 13;
+      Serial.print("ep02");
+    } else if (req.indexOf("/crush/PB5") != -1) {
+      val = 13;
+      Serial.print("ep25");
+    } else
+      /////////////////////////Finishing//////////////////////////
+      if (req.indexOf("/finishing/1") != -1) {
+        val = 1;
+        Serial.print("lp21");
+      } else  if (req.indexOf("/finishing/2") != -1) {
+        val = 2;
+        Serial.print("lp23");
+      } else if (req.indexOf("/finishing/3") != -1) {
+        val = 3;
+        Serial.print("lp25");
+      } else if (req.indexOf("/finishing/4") != -1) {
+        val = 4;
+        Serial.print("lp27");
+      } else if (req.indexOf("/finishing/5") != -1) {
+        val = 5;
+        Serial.print("lp29");
+      } else if (req.indexOf("/finishing/6") != -1) {
+        val = 6;
+        Serial.print("lp31");
+      } else if (req.indexOf("/finishing/7") != -1) {
+        val = 7;
+        Serial.print("lp33");
+      } else if (req.indexOf("/finishing/8") != -1) {
+        val = 8;
+        Serial.print("lp35");
+      } else if (req.indexOf("/finishing/9") != -1) {
+        val = 9;
+        Serial.print("lp37");
+      } else if (req.indexOf("/finishing/A") != -1) {
+        val = 10;
+        Serial.print("lp39");
+      } else if (req.indexOf("/finishing/B") != -1) {
+        val = 11;
+        Serial.print("lp40");
+      }else  if (req.indexOf("/finishing/P4") != -1) {
+    val = 10;
+    Serial.print("ep04");
+  } else  if (req.indexOf("/finishing/P6") != -1) {
+    val = 10;
+    Serial.print("ep06");
+  } else  if (req.indexOf("/finishing/PA8") != -1) {
+    val = 10;
+    Serial.print("ep18");
+  } else
+    ///////////////////////////Retanning////////////////////////
+    if (req.indexOf("/retan/8") != -1) {
+      val = 10;
+      Serial.print("ep33");
+    } else  if (req.indexOf("/retan/1") != -1) {
+      val = 10;
+      Serial.print("ep15");
+    } else  if (req.indexOf("/retan/2") != -1) {
+      val = 10;
+      Serial.print("ep17");
+    } else  if (req.indexOf("/retan/3") != -1) {
+      val = 10;
+      Serial.print("ep19");
+    } else  if (req.indexOf("/retan/4") != -1) {
+      val = 10;
+      Serial.print("ep21");
+    } else  if (req.indexOf("/retan/5") != -1) {
+      val = 10;
+      Serial.print("ep23");
+    } else  if (req.indexOf("/retan/6") != -1) {
+      val = 10;
+      Serial.print("ep28");
+    } else  if (req.indexOf("/retan/7") != -1) {
+      val = 10;
+      Serial.print("ep31");
+    }
+  ///////////////////////////End APP//////////////////////////
+    else {
+      lcd.setCursor(0, 1);
+      lcd.println("invalid request");
+      client.stop();
+      return;
+    }
+
+  // Set GPIO2 according to the request
+  digitalWrite(LED_BUILTIN, i);
+
+  client.flush();
+
+  // Prepare the response
+  String web = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n";
+  web += "<html>\r\n";
+  web += "<body>\r\n";
+  web += "<h1>ECCO Light Control</h1>\r\n";
+  web += "<p></p>";
+  web += "<p>\r\n"; 
+  web += "<p>Please Contact Maintenance Department to Get appication</p>";
+  web += "</body>\r\n";
+  web += "</html>\r\n";
+  client.print(web);
+  delay(50);
+
+  
+
+  // The client will actually be disconnected
+  // when the function returns and 'client' object is detroyed
+
+
+}
+
---
+++ MEGA2560CODE/MEGA2560CODE.ino
@@ -0,0 +1,440 @@
+#include <SimpleModbusMaster.h>
+#include <Wire.h>
+//////////////////// Comment for Valuable ///////////////
+/*
+  Fuction Code for Operation
+    COIL_OFF, 0x0000                    // Function 5 OFF request is 0x0000
+    COIL_ON, 0xFF00                     // Function 5 ON request is 0xFF00
+    READ_COIL_STATUS, 0x01, 1           // Reads the ON/OFF status of discrete outputs (0X references, coils) in the slave.
+    READ_INPUT_STATUS, 0x02, 2          // Reads the ON/OFF status of discrete inputs (1X references) in the slave.
+    READ_HOLDING_REGISTERS, 4x03, 3     // Reads the binary contents of holding registers (4X references) in the slave.
+    READ_INPUT_REGISTERS, 4x04,4        // Reads the binary contents of input registers (3X references) in the slave. Not writable.
+    FORCE_SINGLE_COIL, 0x05, 5          // Forces a single coil (0X reference) to either ON (0xFF00) or OFF (0x0000).
+    PRESET_SINGLE_REGISTER, 4x06, 6     // Presets a value into a single holding register (4X reference).
+    FORCE_MULTIPLE_COILS, 0x15, 15      // Forces each coil (0X reference) in a sequence of coils to either ON or OFF.
+    PRESET_MULTIPLE_REGISTERS, 4x16, 16 // Presets values into a sequence of holding registers (4X references).
+
+*/
+//////////////////// Port information ///////////////////
+#define baud 19200
+#define timeout 300 // Do everything in 1 loop.
+#define polling 200 // the scan rate "200ms For Scanning"
+#define retry_count 10
+//  used to toggle the receive/transmit pin on the driver rs485
+#define TxEnablePin 2
+//  The total amount of available memory on the master to store data
+#define TOTAL_NO_OF_REGISTERS 3
+String val;
+//////////////////////////////// This is the easiest way to create new packets ////////////////////////////////////////////
+//////////////////////////////// Add as many as you want. TOTAL_NO_OF_PACKETS /////////////////////////////////////////////
+/////////////////////////////////////////// is automatically updated. /////////////////////////////////////////////////////
+enum
+{
+  PACKET1,  //  Packet for Stat
+  PACKET2  , PACKET3   , //  Packet for M0
+  TOTAL_NO_OF_PACKETS // leave this last entry
+};
+
+// Create an array of Packets to be configured
+Packet packets[TOTAL_NO_OF_PACKETS];
+// Masters register array
+unsigned int regs[TOTAL_NO_OF_REGISTERS];
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////SETUP/////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+void setup()
+{
+  // Setthing TTL Serial Communication port and Monitor
+  Serial.begin(115200);
+
+  Serial3.begin(115200);
+
+  // Initialize each packet for common negative
+  pinMode(22, OUTPUT); digitalWrite(22, HIGH);
+  pinMode(24, OUTPUT); digitalWrite(24, HIGH);
+  pinMode(26, OUTPUT); digitalWrite(26, HIGH);
+  pinMode(28, OUTPUT); digitalWrite(28, HIGH);
+  pinMode(30, OUTPUT); digitalWrite(30, HIGH);
+  pinMode(32, OUTPUT); digitalWrite(32, HIGH);
+  pinMode(34, OUTPUT); digitalWrite(34, HIGH);
+  pinMode(36, OUTPUT); digitalWrite(36, HIGH);
+
+  // modbus_construct(&packets[PACKET##], slave_adr, function_code, start_registor_adr, number_of_registor, regs_value_array);
+
+
+  // Initialize the Modbus Finite State Machine
+  modbus_configure(&Serial1, baud, SERIAL_8E1, timeout, polling, retry_count, TxEnablePin, packets, TOTAL_NO_OF_PACKETS, regs);
+
+
+}
+
+void loop()
+{
+  regs[1] = COIL_ON;
+  regs[0] = COIL_OFF;
+  if ( Serial3.available())
+  {
+    //////////////////////////////////////////////////////////////////////////////////
+    val = Serial3.readString();
+
+    Serial.print(val);
+    ///////////////////WetBlue////////////////////
+    if (val == "lp01") {
+      digitalWrite(22, LOW);
+      M0();
+      delay(500);
+      digitalWrite(22, HIGH);
+    } else if (val == "lp03") {
+      digitalWrite(24, LOW);
+      M0();
+      delay(500);
+      digitalWrite(24 , HIGH);
+    }  if (val == "lp05") {
+      digitalWrite(26, LOW);
+      M0();
+      delay(500);
+      digitalWrite(26, HIGH);
+    } else if (val == "lp07") {
+      digitalWrite(28, LOW);
+      M0();
+      delay(500);
+      digitalWrite(28, HIGH);
+    } else if (val == "lp09") {
+      digitalWrite(30, LOW);
+      M0();
+      delay(500);
+      digitalWrite(30, HIGH);
+    } else if (val == "lp11") {
+      digitalWrite(32, LOW);
+      M0();
+      delay(500);
+      digitalWrite(32, HIGH);
+    } else if (val == "lp13") {
+      digitalWrite(34, LOW);
+      M0();
+      delay(500);
+      digitalWrite(34, HIGH);
+    } else if (val == "lp15") {
+      digitalWrite(36, LOW);
+      M0();
+      delay(500);
+      digitalWrite(36, HIGH);
+    } else  if (val == "lp17") {
+      digitalWrite(22, LOW);
+      M1();
+      delay(500);
+      digitalWrite(22, HIGH);
+    } else if (val == "lp19") {
+      digitalWrite(24, LOW);
+      M1();
+      delay(500);
+      digitalWrite(24 , HIGH);
+    } else if (val == "ep27") {
+      digitalWrite(26, LOW);
+      M4();
+      delay(500);
+      digitalWrite(26, HIGH);
+    } else if (val == "ep29") {
+      digitalWrite(28, LOW);
+      M4();
+      delay(500);
+      digitalWrite(28, HIGH);
+    } else
+      /////////////////Crust////////////////////////
+      if (val == "lp02") {
+        digitalWrite(26, LOW);
+        M1();
+        delay(500);
+        digitalWrite(26, HIGH);
+      } else if (val == "lp04") {
+        digitalWrite(28, LOW);
+        M1();
+        delay(500);
+        digitalWrite(28, HIGH);
+      } else if (val == "lp06") {
+        digitalWrite(30, LOW);
+        M1();
+        delay(500);
+        digitalWrite(30, HIGH);
+      } else if (val == "lp08") {
+        digitalWrite(32, LOW);
+        M1();
+        delay(500);
+        digitalWrite(32, HIGH);
+      } else if (val == "lp10") {
+        digitalWrite(34, LOW);
+        M1();
+        delay(500);
+        digitalWrite(34, HIGH);
+      } else if (val == "lp12") {
+        digitalWrite(36, LOW);
+        M1();
+        delay(500);
+        digitalWrite(36, HIGH);
+      } else if (val == "lp14") {
+        digitalWrite(22, LOW);
+        M2();
+        delay(500);
+        digitalWrite(22, HIGH);
+      } else if (val == "lp16") {
+        digitalWrite(24, LOW);
+        M2();
+        delay(500);
+        digitalWrite(24, HIGH);
+      } else if (val == "lp18") {
+        digitalWrite(26, LOW);
+        M2();
+        delay(500);
+        digitalWrite(26, HIGH);
+      } else if (val == "lp28") {
+        digitalWrite(28, LOW);
+        M2();
+        delay(500);
+        digitalWrite(28, HIGH);
+      } else if (val == "lp32") {
+        digitalWrite(30, LOW);
+        M2();
+        delay(500);
+        digitalWrite(30, HIGH);
+      } else if (val == "lp38") {
+        digitalWrite(32, LOW);
+        M2();
+        delay(500);
+        digitalWrite(32, HIGH);
+      } else if (val == "lp41") {
+        digitalWrite(34, LOW);
+        M2();
+        delay(500);
+        digitalWrite(34, HIGH);
+      } else if (val == "ep02") {
+        digitalWrite(30, LOW);
+        M4();
+        delay(500);
+        digitalWrite(30, HIGH);
+      } else if (val == "ep25") {
+        digitalWrite(32, LOW);
+        M4();
+        delay(500);
+        digitalWrite(32, HIGH);
+      } else
+        //////////////////Finishing///////////////////
+        if (val == "lp21") {
+          digitalWrite(36, LOW);
+          M2();
+          delay(500);
+          digitalWrite(36, HIGH);
+        } else if (val == "lp23") {
+          digitalWrite(22, LOW);
+          M3();
+          delay(500);
+          digitalWrite(22, HIGH);
+        } else if (val == "lp25") {
+          digitalWrite(24, LOW);
+          M3();
+          delay(500);
+          digitalWrite(24, HIGH);
+        } else if (val == "lp27") {
+          digitalWrite(26, LOW);
+          M3();
+          delay(500);
+          digitalWrite(26, HIGH);
+        } else if (val == "lp29") {
+          digitalWrite(28, LOW);
+          M3();
+          delay(500);
+          digitalWrite(28, HIGH);
+        } else if (val == "lp31") {
+          digitalWrite(30, LOW);
+          M3();
+          delay(500);
+          digitalWrite(30, HIGH);
+        } else if (val == "lp33") {
+          digitalWrite(32, LOW);
+          M3();
+          delay(500);
+          digitalWrite(32, HIGH);
+        } else if (val == "lp35") {
+          digitalWrite(34, LOW);
+          M3();
+          delay(500);
+          digitalWrite(34, HIGH);
+        } else if (val == "lp37") {
+          digitalWrite(36, LOW);
+          M3();
+          delay(500);
+          digitalWrite(36, HIGH);
+        } else if (val == "lp39") {
+          digitalWrite(22, LOW);
+          M4();
+          delay(500);
+          digitalWrite(22, HIGH);
+        } else if (val == "lp40") {
+          digitalWrite(24, LOW);
+          M4();
+          delay(500);
+          digitalWrite(24, HIGH);
+        } else if (val == "ep04") {
+          digitalWrite(34, LOW);
+          M4();
+          delay(500);
+          digitalWrite(34, HIGH);
+        } else if (val == "ep06") {
+          digitalWrite(36, LOW);
+          M4();
+          delay(500);
+          digitalWrite(36, HIGH);
+        } else if (val == "ep18") {
+          digitalWrite(22, LOW);
+          M5();
+          delay(500);
+          digitalWrite(22, HIGH);
+        } else
+          ///////////////////Retanning//////////////////
+          if (val == "ep15") {
+            digitalWrite(24, LOW);
+            M5();
+            delay(500);
+            digitalWrite(24, HIGH);
+          } else if (val == "ep17") {
+            digitalWrite(26, LOW);
+            M5();
+            delay(500);
+            digitalWrite(26, HIGH);
+          } else if (val == "ep19") {
+            digitalWrite(28, LOW);
+            M5();
+            delay(500);
+            digitalWrite(28, HIGH);
+          } else if (val == "ep21") {
+            digitalWrite(30, LOW);
+            M5();
+            delay(500);
+            digitalWrite(30, HIGH);
+          } else if (val == "ep23") {
+            digitalWrite(32, LOW);
+            M5();
+            delay(500);
+            digitalWrite(32, HIGH);
+          } else if (val == "ep28") {
+            digitalWrite(34, LOW);
+            M5();
+            delay(500);
+            digitalWrite(34, HIGH);
+          } else if (val == "ep31") {
+            digitalWrite(36, LOW);
+            M5();
+            delay(500);
+            digitalWrite(36, HIGH);
+          } else if (val == "ep33") {
+            digitalWrite(22, LOW);
+            M6();
+            delay(500);
+            digitalWrite(22, HIGH);
+          } 
+            /////////////////Ending LOOP//////////////////
+            else {}
+  }
+}
+void modbus_update10() {
+  modbus_update();
+  delay(10);
+  modbus_update();
+  delay(10);
+  modbus_update();
+  delay(10);
+  modbus_update();
+  delay(10);
+  modbus_update();
+  delay(10);
+  modbus_update();
+  delay(10);
+
+}
+
+void modbus_update40() {
+  modbus_update();
+  delay(40);
+  modbus_update();
+  delay(40);
+  modbus_update();
+  delay(40);
+  modbus_update();
+  delay(40);
+  modbus_update();
+  delay(40);
+  modbus_update();
+  delay(40);
+}
+//////////////// Triker Signal for scan time///////////////
+void M0()
+{
+  modbus_construct(&packets[PACKET3], 1, FORCE_SINGLE_COIL, 0, 1, 1);
+  modbus_update10();
+  delay(50);
+  modbus_construct(&packets[PACKET2], 1, FORCE_SINGLE_COIL, 0, 1, 0);
+
+  modbus_update40();
+}
+
+void M1()
+{
+  modbus_construct(&packets[PACKET3], 1, FORCE_SINGLE_COIL, 1, 1, 1);
+  modbus_update10();
+  delay(50);
+  modbus_construct(&packets[PACKET2], 1, FORCE_SINGLE_COIL, 1, 1, 0);
+  modbus_update40();
+}
+void M2()
+{
+  modbus_construct(&packets[PACKET3], 1, FORCE_SINGLE_COIL, 2, 1, 1);
+  modbus_update10();
+  delay(50);
+  modbus_construct(&packets[PACKET2], 1, FORCE_SINGLE_COIL, 2, 1, 0);
+  modbus_update40();
+}
+
+void M3()
+{
+  modbus_construct(&packets[PACKET3], 1, FORCE_SINGLE_COIL, 3, 1, 1);
+  modbus_update10();
+  delay(50);
+  modbus_construct(&packets[PACKET2], 1, FORCE_SINGLE_COIL, 3, 1, 0);
+  modbus_update40();
+}
+
+void M4()
+{
+  modbus_construct(&packets[PACKET3], 1, FORCE_SINGLE_COIL, 4, 1, 1);
+  modbus_update10();
+  delay(50);
+  modbus_construct(&packets[PACKET2], 1, FORCE_SINGLE_COIL, 4, 1, 0);
+  modbus_update40();
+}
+
+void M5()
+{
+  modbus_construct(&packets[PACKET3], 1, FORCE_SINGLE_COIL, 5, 1, 1);
+  modbus_update10();
+  delay(50);
+  modbus_construct(&packets[PACKET2], 1, FORCE_SINGLE_COIL, 5, 1, 0);
+  modbus_update40();
+}
+void M6()
+{
+  modbus_construct(&packets[PACKET3], 1, FORCE_SINGLE_COIL, 6, 1, 1);
+  modbus_update10();
+  delay(50);
+  modbus_construct(&packets[PACKET2], 1, FORCE_SINGLE_COIL, 6, 1, 0);
+  modbus_update40();
+}
+
+void M7()
+{
+  modbus_construct(&packets[PACKET3], 1, FORCE_SINGLE_COIL, 7, 1, 1);
+  modbus_update10();
+  delay(50);
+  modbus_construct(&packets[PACKET2], 1, FORCE_SINGLE_COIL, 7, 1, 0);
+  modbus_update40();
+}
+
